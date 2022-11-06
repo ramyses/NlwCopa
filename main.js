9eb168b65cf7c60c8db935593e37a2da11faa1aa
ramyses <lukasramysesleao@gmail.com>
2022-11-06 19:41:42 -0400

    refatorando html e js

--- main.js
+++ main.js
@@ -18,25 +18,7 @@ function createCard(date, day,games){
   </div>
                `
 }
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="/assets/logo.svg" alt="">
-  </header>
-        <main id="cards">
-            ${createCard(
-              "24/11",
-              "quinta",
-              createGame("brazil", "16:00", "serbia")
-            )}
-            ${createCard(
-              "28/11",
-              "segunda",
-              createGame("brazil", "13:00", "switzerland")
-            )}
-            ${createCard(
-              "02/12",
-              "sexta",
-              createGame("brazil", "16:00", "serbia")
-            )}
-        </main>
-`
+document.querySelector("#cards").innerHTML = 
+  createCard("24/11","quinta",createGame("brazil","16:00", "serbia")) +
+  createCard("28/11","segunda",createGame("brazil", "13:00", "switzerland") + createGame("portugal", "16:00","urugai")) +
+  createCard("02/12","sexta",createGame("brazil", "16:00", "serbia"));
